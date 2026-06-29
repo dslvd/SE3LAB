0b19997c91c5145ea32c92de7c8df4a5675b5dc5
Nelsocated <lagonelson77@gmail.com>
2026-06-29 12:54:43 +0800

    Fix: sidebar buttons

--- src/script/ui.js
+++ src/script/ui.js
@@ -15,37 +15,23 @@ function SideBar() {
     <a id="overview" href="${homeLink}" class="block px-3 py-1 hover:bg-primary rounded-2xl">
       Overview
     </a>
-    <div id="member" class="px-3 py-1 hover:bg-primary rounded-2xl flex justify-between">
+    <button type="button" id="member" class="px-3 py-1 w-full hover:bg-primary rounded-2xl flex justify-between cursor-pointer">
       Members <img src="../../public/drop.svg" />
-    </div>
+    </button>
 
-    <div id="drop" class="hidden space-y-1">
-      <div id="matt" class="px-5 py-1 ml-5 hover:bg-primary rounded-2xl font-normal text-lg">Matthew</div>
-      <div id="just" class="px-5 py-1 ml-5 hover:bg-primary rounded-2xl font-normal text-lg">Justin</div>
-      <div id="nels" class="px-5 py-1 ml-5 hover:bg-primary rounded-2xl font-normal text-lg">Nelson</div>
+    <div id="drop" class="hidden space-y-1 w-full">
+      <a href="/src/members/matthew.html" class="block px-5 py-1 ml-5 hover:bg-primary rounded-2xl font-normal text-lg">Matthew</a>
+      <a href="/src/members/justin.html" class="block px-5 py-1 ml-5 hover:bg-primary rounded-2xl font-normal text-lg">Justin</a>
+      <a href="/src/members/nelson.html" class="block px-5 py-1 ml-5 hover:bg-primary rounded-2xl font-normal text-lg">Nelson</a>
     </div>
   </aside>
   `;
 }
 
 sidebar.innerHTML = SideBar();
-const overview = document.getElementById("overview");
 const member = document.getElementById("member");
 const drop = document.getElementById("drop");
-const nelson = document.getElementById("nels");
-const justin = document.getElementById("just");
-const matthew = document.getElementById("matt");
 
 member.addEventListener("click", () => {
   drop.classList.toggle("hidden");
-});
-
-nelson.addEventListener("click", () => {
-  window.location.href = "/src/members/nelson.html";
-});
-justin.addEventListener("click", () => {
-  window.location.href = "/src/members/justin.html";
-});
-matthew.addEventListener("click", () => {
-  window.location.href = "/src/members/matthew.html";
 });
